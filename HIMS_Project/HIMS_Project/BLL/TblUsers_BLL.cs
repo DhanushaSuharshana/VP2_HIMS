1e1b9239cf2de7018cea9076958961ffcf7c6a71
sachini yureaka <yureaka97@gmail.com>
2021-09-07 20:30:40 -0700

    test

---
+++ HIMS_Project/HIMS_Project/BLL/TblUsers_BLL.cs
@@ -0,0 +1,26 @@
+﻿using HIMS_Project.DAL;
+using HIMS_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIMS_Project.BLL
+{
+    class TblUsers_BLL
+    {
+        public void LoadUserId(TblUsers tblUser)
+        {
+            try
+            {
+                return TblUsers_DAL.GetUserByUsername(tblUser);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ HIMS_Project/HIMS_Project/Model/TblUsers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS_Project.Model
+{
+    //Encapsulate GET | SET Method
+    class TblUsers
+    {
+        public int UserId { get; set; }
+        public string NIC { get; set; }
+        public string Name { get; set; }
+        public string UserAddress { get; set; }
+        public string DOB { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public int Contact { get; set; }
+        public string Username { get; set; }
+        public string UPassword { get; set; }
+        public string BloodGroup { get; set; }
+        public string Allergies { get; set; }
+        public int UserRole { get; set; }
+        public string StaffId { get; set; }
+
+    }
+}
